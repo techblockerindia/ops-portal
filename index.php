bb3e12ad0ee9a4854d36ffad7cbc510aa1d06ae4
techblockerindia <tech.blockerindia@gmail.com>
2025-03-03 12:23:06 +0000

    .

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>
-
+<title>Blocker India</title>
 <!-- Categories Section -->
 <section id="categories" class="container mx-auto py-16">
   <h2 class="text-3xl font-bold text-center text-gray-800 mb-10">Our Team Categories</h2>
